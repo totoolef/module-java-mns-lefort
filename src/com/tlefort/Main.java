a1e3e6ef5e37b5084115d8caf467376101633390
Thomas LEFORT <thomaslefort@MacBook-Pro-de-Thomas.local>
2025-07-29 11:02:53 +0200

    TP1, what's your name ?

---
+++ src/com/tlefort/Main.java
@@ -0,0 +1,12 @@
+package com.tlefort;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("What's your name ?");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
