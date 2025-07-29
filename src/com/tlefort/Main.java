ab5b7de36b18e7e32bf971c6766360e196a7ec56
Thomas LEFORT <thomaslefort@MacBook-Pro-de-Thomas.local>
2025-07-29 11:16:53 +0200

    TP2, ajouter une tache

--- src/com/tlefort/Main.java
+++ src/com/tlefort/Main.java
@@ -8,5 +8,24 @@ public class Main {
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
         System.out.println("Hello, " + name);
+
+        String[] tasks2 = new String[]{"test2", "test3","test4", "test5"};
+
+        for (String task : tasks2) {
+            System.out.println(task);
+        }
+
+        String[] tasks3 = new String[tasks2.length+1];
+        System.arraycopy(tasks2, 0, tasks3, 0, tasks2.length);
+        System.out.println("Ajouter une tache");
+        String task = input.nextLine();
+        input.close();
+
+        tasks3[tasks3.length-1] = task;
+
+        for (String t: tasks3) {
+            System.out.println(t);
+        }
+
     }
 }
